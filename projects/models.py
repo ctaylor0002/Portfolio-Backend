3e3de83ca736959c3b970c10078a9be48b1db822
Charles Taylor <charles4312@comcast.net>
2023-05-07 00:05:03 -0400

    Created 'Project' Model + Migration

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=50)
+    project_link = models.CharField(max_length=100)
+    project_desc = models.CharField(max_length=2500)
+
+
+    # Add this when I add the pillow dependency
+    # project_screenshot = models.ImageField(upload_to="images/", default='../images/missing_image.jpg')
