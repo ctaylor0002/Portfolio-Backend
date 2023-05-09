f7502cd50a374f2ae9fe519757955f7294fca972
Charles Taylor <charles4312@comcast.net>
2023-05-08 22:25:48 -0400

    Added Endpoint To Retrieve All Projects

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from .models import Project
+from .serializers import ProjectSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
 
-# Create your views here.
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_projects(request):
+    projects = Project.objects.all()
+    serializer = ProjectSerializer(projects, many=True)
+    return Response(serializer.data)
